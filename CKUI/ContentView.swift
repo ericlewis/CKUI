60b4271d54f27d6ede7e55c2d481b0eb707ecaa5
Eric Lewis <ericlewis777@gmail.com>
2020-03-29 15:34:24 -0400

    wrappers and wrappers

--- CKUI/ContentView.swift
+++ CKUI/ContentView.swift
@@ -1,17 +1,9 @@
-//
-//  ContentView.swift
-//  CKUI
-//
-//  Created by Eric Lewis on 3/28/20.
-//  Copyright © 2020 Eric Lewis. All rights reserved.
-//
-
 import SwiftUI
 import CareKit
 
 struct ContentView: View {
     var body: some View {
-        SimpleTaskWrapper(taskID: "doxylaminee", manager: manager)  { event, markComplete in
+        SimpleTask(taskID: "doxylaminee") { event, markComplete in
             Text(event?.task.title ?? "no task")
             Button(event?.outcome == nil ? "Mark as Completed" : "Completed",
                    action: markComplete)
--- CKUI/SimpleTaskWrapper.swift
+++ CKUI/SimpleTaskWrapper.swift
@@ -1,6 +1,54 @@
 import SwiftUI
 import CareKit
 
+struct SimpleTask<Content: View>: View {
+    let taskID: String
+    let eventQuery: OCKEventQuery
+    let content: SimpleTaskWrapper<Content>.ContentFunc
+    
+    init(taskID: String,
+         eventQuery: OCKEventQuery = OCKEventQuery(for: Date()),
+         @ViewBuilder content: @escaping SimpleTaskWrapper<Content>.ContentFunc) {
+        self.taskID = taskID
+        self.eventQuery = eventQuery
+        self.content = content
+    }
+    
+    
+    var body: some View {
+        ManagerWrapper {
+            SimpleTaskWrapper(taskID: self.taskID,
+                              eventQuery: self.eventQuery,
+                              manager: $0,
+                              content: self.content)
+        }
+    }
+}
+
+struct ManagerWrapper<Content: View>: View {
+    @Environment(\.manager) var manager: OCKSynchronizedStoreManager
+    var content: (OCKSynchronizedStoreManager) -> Content
+    
+    var body: some View {
+        content(manager)
+    }
+}
+
+extension EnvironmentValues {
+    var manager: OCKSynchronizedStoreManager {
+        get {
+            return self[T.self]
+        }
+        set {
+            self[T.self] = newValue
+        }
+    }
+}
+
+public struct T: EnvironmentKey {
+    public static let defaultValue: OCKSynchronizedStoreManager = OCKSynchronizedStoreManager(wrapping: store)
+}
+
 struct SimpleTaskWrapper<Content: View>: View {
     typealias ContentFunc = (OCKAnyEvent?, @escaping () -> Void) -> Content
     let content: ContentFunc
@@ -13,7 +61,7 @@ struct SimpleTaskWrapper<Content: View>: View {
     init(taskID: String,
          eventQuery: OCKEventQuery = OCKEventQuery(for: Date()),
          manager: OCKSynchronizedStoreManager,
-         @ViewBuilder content: @escaping ContentFunc) {
+         content: @escaping ContentFunc) {
         self.content = content
         self.controller = OCKSimpleTaskController(storeManager: manager)
         controller.fetchAndObserveEvents(forTaskID: taskID,
